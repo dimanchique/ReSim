9bf40249d76876c4b4e877af682a6db494ca42cb
dimanchique <ghettorunner@yandex.ru>
2023-10-23 21:05:32 +0300

    test CRC-16 calculation asm program result verified

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "CPU6502.h"
 #include <chrono>
 #include <iostream>
+#include <cassert>
 
 static std::vector<char> ReadAllBytes(char const* filename)
 {
@@ -26,15 +27,21 @@ int main() {
     CPU6502 cpu{};
     Memory mem{64};
 
-    cpu.Reset(mem, 0x0000);
+    cpu.Reset(mem, 0x3000);
     for(auto i = 0; i < data.size(); i++)
-        mem[0x0000 + i] = data[i];
+        mem[0x3000 + i] = data[i];
+
+    for(auto i = 0x1000; i < 0x1100; i++)
+        mem[i] = 0x02;
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     const auto n = cpu.Run(mem);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
     const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+
+    assert(mem[0x06] == 0xA1);
+    assert(mem[0x07] == 0xF1);
 
     std::cout << "Execution duration: " << duration / 1e6 << "[s]" << std::endl;
     std::cout << "Cycles count: " << n  << std::endl;
