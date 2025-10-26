2202676ff3a4e5f652acd07b7c599501fbea7575
Dmitriy Filippov <ghettorunner@yandex.ru>
2025-10-26 14:10:57 +0300

    [I8086] MOV Ex Ix instruction

--- chips/I8086/inc/Operations/MOV_Imm_Ops.h
+++ chips/I8086/inc/Operations/MOV_Imm_Ops.h
@@ -81,3 +81,17 @@ void I8086_MOV_SI_Iv(BYTE OpCode, I8086 &cpu) {
 void I8086_MOV_DI_Iv(BYTE OpCode, I8086 &cpu) {
     I8086_MOV_Iv(OpCode, cpu);
 }
+
+//  Reg <-- Immediate8
+void I8086_MOV_Eb_Ib(BYTE OpCode, I8086 &cpu) {
+    const BYTE modByte = cpu.Fetch<BYTE>();
+    const ModRegByte modReg = ModRegByte::FromByte(modByte);
+    I8086_Ex_Ix<BYTE>(cpu, modReg, &PerformMOV, &UpdateStatusAfterMOV_Wrapper);
+}
+
+//  Reg <-- Immediate16
+void I8086_MOV_Ev_Iv(BYTE OpCode, I8086 &cpu) {
+    const BYTE modByte = cpu.Fetch<BYTE>();
+    const ModRegByte modReg = ModRegByte::FromByte(modByte);
+    I8086_Ex_Ix<WORD>(cpu, modReg, &PerformMOV, &UpdateStatusAfterMOV_Wrapper);
+}
