d36468d969be687b3e4086fd755f622b85cdc0c2
Dmitriy Filippov <ghettorunner@yandex.ru>
2025-10-27 22:35:27 +0300

    [I8086] LES/LDS/LEA tests

---
+++ tests/I8086/src/LOAD_Tests.cpp
@@ -0,0 +1,102 @@
+#include "I8086_TestingSuite.h"
+#include "I8086_OpCodes_Groups.h"
+
+TEST_F(I8086_TestFixture, LOAD_Gv_M) {
+    // given:
+    cpu.PC = 0x1000;
+    cpu.SP = 0x6000;
+
+    cpu.DS = 0x2800;
+    cpu.BX = 0x0400;
+    cpu.SI = 0x003C;
+
+    ModRegByteConstructor modReg;
+
+    modReg.leftOp.archetype = OperandArchetype::Reg;
+    modReg.leftOp.regData = wBX;
+
+    modReg.rightOp.archetype = OperandArchetype::Mem;
+    modReg.rightOp.memData.mode = modeBXpSI;
+    modReg.rightOp.memData.dispSize = 2;
+    modReg.rightOp.memData.dispValue = 0x0F62;
+
+    mem[effectiveAddress++] = LEA_Gv_M;
+    mem[effectiveAddress++] = modReg.MakeModByte();
+    mem[effectiveAddress++] = 0x62;
+    mem[effectiveAddress++] = 0x0F;
+    mem[effectiveAddress++] = I8086_STOP_OPCODE;
+
+    // when:
+    cyclesPassed = cpu.Run();
+
+    // then:
+    EXPECT_EQ(cpu.BX, 0x139E);
+}
+
+TEST_F(I8086_TestFixture, LES_Gv_Mp) {
+    // given:
+    cpu.PC = 0x1000;
+    cpu.SP = 0x6000;
+
+    cpu.DS = 0x2800;
+    cpu.BX = 0x0400;
+    cpu.SI = 0x003C;
+
+    ModRegByteConstructor modReg;
+
+    modReg.leftOp.archetype = OperandArchetype::Reg;
+    modReg.leftOp.regData = wBX;
+
+    modReg.rightOp.archetype = OperandArchetype::Mem;
+    modReg.rightOp.memData.mode = modeBXpSI;
+    modReg.rightOp.memData.dispSize = 2;
+    modReg.rightOp.memData.dispValue = 0x0F62;
+
+    mem[effectiveAddress++] = LES_Gv_Mp;
+    mem[effectiveAddress++] = modReg.MakeModByte();
+    mem[effectiveAddress++] = 0x62;
+    mem[effectiveAddress++] = 0x0F;
+    mem[effectiveAddress++] = I8086_STOP_OPCODE;
+
+    // when:
+    cyclesPassed = cpu.Run();
+
+    // then:
+    // EA here is 0x2939E
+    EXPECT_EQ(cpu.BX, 0x0002);
+    EXPECT_EQ(cpu.ES, 0x939E);
+}
+
+TEST_F(I8086_TestFixture, LDS_Gv_Mp) {
+    // given:
+    cpu.PC = 0x1000;
+    cpu.SP = 0x6000;
+
+    cpu.DS = 0x2800;
+    cpu.BX = 0x0400;
+    cpu.SI = 0x003C;
+
+    ModRegByteConstructor modReg;
+
+    modReg.leftOp.archetype = OperandArchetype::Reg;
+    modReg.leftOp.regData = wBX;
+
+    modReg.rightOp.archetype = OperandArchetype::Mem;
+    modReg.rightOp.memData.mode = modeBXpSI;
+    modReg.rightOp.memData.dispSize = 2;
+    modReg.rightOp.memData.dispValue = 0x0F62;
+
+    mem[effectiveAddress++] = LDS_Gv_Mp;
+    mem[effectiveAddress++] = modReg.MakeModByte();
+    mem[effectiveAddress++] = 0x62;
+    mem[effectiveAddress++] = 0x0F;
+    mem[effectiveAddress++] = I8086_STOP_OPCODE;
+
+    // when:
+    cyclesPassed = cpu.Run();
+
+    // then:
+    // EA here is 0x2939E
+    EXPECT_EQ(cpu.BX, 0x0002);
+    EXPECT_EQ(cpu.DS, 0x939E);
+}
